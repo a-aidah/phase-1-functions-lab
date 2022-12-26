3eaebbfa8057624c18ce80d03d6bf3f7513289b8
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-26 21:10:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    let blockDistance;
+    if (pickupLocation > 42) {
+        blockDistance = pickupLocation - 42;
+    } else {
+        blockDistance = 42 - pickupLocation;
+    }
+    return blockDistance;
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceInFeet;
+    if (start > destination) {
+        distanceInFeet = (start - destination) * 264;
+    } else {
+        distanceInFeet = (destination - start) * 264;
+    }
+    return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    let farePrice;
+    if (feetDistance <= 400) {
+        farePrice = 0;
+    } else if (feetDistance <= 2000) {
+        farePrice = (feetDistance - 400) * 0.02;
+    } else if (feetDistance <= 2500) {
+        farePrice = 25;
+    }
+    else {
+        farePrice = "cannot travel that far";
+    }
+    return farePrice;
+}
